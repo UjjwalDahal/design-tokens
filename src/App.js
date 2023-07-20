4bea9e6a79e00324384fe7f45623cb5021a7efa5
Ujjwal Dahal <ujjwaldahal35@gmail.com>
2023-07-20 17:29:20 +0545

    working on configuring grid with figma

--- src/App.js
+++ src/App.js
@@ -19,28 +19,16 @@ const App = () => {
       </div>
 
       <div className="app_wrapper">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam voluptate
-        provident culpa nemo consequatur consectetur laborum neque beatae soluta
-        quas sequi libero dolore quibusdam voluptates placeat sit illum vero
-        accusamus aperiam voluptatum quae, sapiente aspernatur? Delectus
-        voluptas fugiat, unde recusandae maxime saepe harum minus earum
-        provident veniam vitae ducimus ipsum eaque, tenetur repudiandae adipisci
-        dicta animi explicabo! Dolorem necessitatibus ab repellat veniam! Minima
-        voluptas minus quidem perspiciatis libero blanditiis vel aut? Minus
-        illum officia hic? Atque dignissimos maxime numquam pariatur quam
-        facilis odio accusantium assumenda fugit harum aspernatur earum quos, in
-        iste expedita ipsam officiis, repellat vitae ducimus quas! Nobis
-        incidunt rem rerum labore blanditiis similique, vel, officiis laborum
-        consectetur tenetur ab obcaecati ipsa quae eos laudantium? Laboriosam
-        velit magni commodi ipsum sunt nisi officiis! Consequuntur fugit harum
-        nihil culpa numquam soluta quos delectus tempore autem amet recusandae
-        sunt consequatur, laborum dolorem quaerat tempora ipsum aspernatur rem
-        voluptas quod, omnis facilis doloremque asperiores. Inventore dicta sit
-        sed aperiam quo laboriosam fugit officia, voluptatum culpa eius dolorem
-        perferendis. Mollitia necessitatibus earum quod dicta commodi aut veniam
-        excepturi voluptate, vitae architecto obcaecati voluptatum quasi odio
-        delectus ducimus illum dolor pariatur magni quidem explicabo. Delectus
-        repellat eos consequuntur minus exercitationem libero dolore sapiente?
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </>
   );
